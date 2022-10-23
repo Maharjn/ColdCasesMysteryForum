2cd248f662204f7310c45a09098fa76e593e2506
Maha <maha.spearmint@gmail.com>
2022-10-23 20:00:06 +0800

    react bootstrap navbar added

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,44 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Nav, Navbar } from 'react-bootstrap';
 import Auth from '../../utils/auth';
-
+import Container from 'react-bootstrap/Container';
+import './Nav.css';
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">Cold Cases and Unsolved Mysteries</h1>
+    <header>
+     
+      
+          <Link  to="/">
+            <h1 >Cold Cases and Unsolved Mysteries</h1>
           </Link>
-          <p className="m-0">Detective mind Thouhts.</p>
-        </div>
-        <div>
+          <p className='p-item'>Detective mind Thouhts.</p>
+<Navbar>
+<Container>
+  <Nav>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
+<Nav.Item className="ms-auto">
+              <Nav.Link  to="/me">
                 {Auth.getProfile().data.username}'s profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              </Nav.Link>
+              <a href="/" onClick={logout}>
                 Logout
-              </button>
+              </a>
+              </Nav.Item>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+            <Nav.Item>
+              <Nav.Link  to="/login">
                 Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              </Nav.Link>
+              <Nav.Link  to="/signup">
                 Signup
-              </Link>
+              </Nav.Link>
+              </Nav.Item>
             </>
           )}
-        </div>
-      </div>
+          </Nav>
+           </Container>
+</Navbar>
     </header>
   );
 };
